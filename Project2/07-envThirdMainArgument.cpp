f8089b705c0ff03014a3d6a3f9f9eee480e1be39
Jie Yu <jyu238@myseneca.ca>
2024-12-16 17:00:22 -0500

    explanations of three arguments

--- Project2/07-envThirdMainArgument.cpp
+++ Project2/07-envThirdMainArgument.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 //project2		this is a test
+
+/*
+argc stands for "argument count"
+argv stands for "argument vector"
+env stands for "environment variables"
+
+this allows us to access to command-line arguments and environment variables
+*/
 int main(int argc, char* argv[], char* env[]) {
 	for (int i = 0; env[i]; i++) {
 		cout << i+1 << "- " << env[i]<< endl;
